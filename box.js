03807c58faf5cc7e8f1b941be9a9581b221a3e26
prakhar44-coder <70267931+prakhar44-coder@users.noreply.github.com>
2020-09-22 08:57:17 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,20 @@
+class Rectangle {
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true,
+          density:2.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
